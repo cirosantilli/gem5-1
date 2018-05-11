5b5432deae2d5867c46cf9fb1d48cd597f7bf1eb
Ciro Santilli <ciro.santilli@gmail.com>
2018-05-11 08:18:27 +0100

    arm 64 cpus
    https://stackoverflow.com/questions/50248067/how-to-run-a-gem5-arm-aarch64-full-system-simulation-with-fs-py-with-more-than-8/50248068#50248068
    
    Change-Id: Ia9bf95a3f120d7fd8000e6ea6b7b20a4c0b999a5

--- configs/example/fs.py
+++ configs/example/fs.py
@@ -395,5 +395,6 @@ if buildEnv['TARGET_ISA'] == "arm" and options.generate_dtb:
             sys = getattr(root, sysname)
             sys.dtb_filename = create_dtb_for_system(sys, '%s.dtb' % sysname)
 
+root.system.realview.gic.gem5_extensions = True
 Simulation.setWorkCountOptions(test_sys, options)
 Simulation.run(options, root, test_sys, FutureClass)
